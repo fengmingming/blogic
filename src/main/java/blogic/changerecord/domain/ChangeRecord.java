ff5680538cf331406b45e5eb6ead07d643a2e08a
冯明明 <fengmingming@users.noreply.github.com>
2024-01-30 16:06:42 +0800

    重新定义ChangeRecord.java

--- src/main/java/blogic/changerecord/domain/ChangeRecord.java
+++ src/main/java/blogic/changerecord/domain/ChangeRecord.java
@@ -3,7 +3,8 @@ package blogic.changerecord.domain;
 import blogic.changerecord.domain.repository.ChangeRecordRepository;
 import blogic.core.context.SpringContext;
 import blogic.core.domain.ActiveRecord;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -14,9 +15,6 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @Table("change_record")
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class ChangeRecord extends ActiveRecord<ChangeRecord, Long> {
 
     @Id
@@ -42,6 +40,66 @@ public class ChangeRecord extends ActiveRecord<ChangeRecord, Long> {
     @Override
     protected <S extends ChangeRecord> S selfS() {
         return (S) this;
+    }
+
+    public static ChangeRecordBuilder builder() {
+        return new ChangeRecordBuilder();
+    }
+
+    public static class ChangeRecordBuilder {
+        private Long id;
+        private Long primaryKey;
+        private Integer keyType;
+        private Long operUserId;
+        private String operDesc;
+        private String note;
+        private LocalDateTime createTime;
+
+        public ChangeRecordBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ChangeRecordBuilder primaryKey(Long primaryKey) {
+            this.primaryKey = primaryKey;
+            return this;
+        }
+
+        public ChangeRecordBuilder keyType(Integer keyType) {
+            this.keyType = keyType;
+            return this;
+        }
+
+        public ChangeRecordBuilder operUserId(Long operUserId) {
+            this.operUserId = operUserId;
+            return this;
+        }
+
+        public ChangeRecordBuilder operDesc(String operDesc) {
+            this.operDesc = operDesc;
+            return this;
+        }
+
+        public ChangeRecordBuilder note(String note) {
+            this.note = note;
+            return this;
+        }
+
+        public ChangeRecordBuilder createTime(LocalDateTime createTime) {
+            this.createTime = createTime;
+            return this;
+        }
+
+        public ChangeRecord build() {
+            ChangeRecord record = new ChangeRecord();
+            record.setKeyType(keyType);
+            record.setPrimaryKey(primaryKey);
+            record.setOperDesc(operDesc);
+            record.setNote(note);
+            record.setOperUserId(operUserId);
+            record.setCreateTime(createTime);
+            return record;
+        }
     }
 
 }
